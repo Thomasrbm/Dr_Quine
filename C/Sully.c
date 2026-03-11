5790bc51b9c88a847524a727d804f28a7a7cc97b
Thomasrbm <thomas.robert76@hotmail.com>
2026-03-11 02:50:44 +0100

    d

---
+++ C/Sully.c
@@ -0,0 +1,17 @@
+The executable must be named Sully.
+
+
+• When executed the program writes in a file named Sully_X.c / Sully_X.s. The X
+will be an integer given in the source. Once the file is created, the program compiles
+this file and then runs the new program (which will have the name of its source file).
+
+• Stopping the program depends on the file name: the resulting program will be ex-
+ecuted only if the integer X is greater or equals than 0.
+
+• An integer is therefore present in the source of your program and will have to evolve
+by decrementing every time you create a source file from the execution of the pro-
+gram.
+
+
+• You have no constraints on the source code, apart from the integer that will be set
+to 5 at first.
