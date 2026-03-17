e9fc2de385cd1e22a3a1a4f7c13b049bf85615e2
Thomasrbm <frozyxyt.76@gmail.com>
2026-03-17 07:46:31 +0100

    Fix: cleaner sully

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,125 +1,68 @@
-extern fopen
-extern fprintf
-extern fclose
-extern system
-extern sprintf
-
-NL    equ 10
-QUOTE equ 34
-TAB   equ 9
+; i = 5
+extern fopen, fprintf, fclose, sprintf, access, system
 
 section .data
-	mode		db "w", 0
-	fname_fmt	db "Sully_%d.s", 0
-	cmd_fmt		db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o && cc -no-pie Sully_%1$d.o -o Sully_%1$d && rm Sully_%1$d.o", 0
-	exec_fmt	db "./Sully_%d", 0
-	string		db "extern fopen%1$cextern fprintf%1$cextern fclose%1$cextern system%1$cextern sprintf%1$c%1$cNL    equ 10%1$cQUOTE equ 34%1$cTAB   equ 9%1$c%1$csection .data%1$c%5$cmode%5$c%5$cdb %2$cw%2$c, 0%1$c%5$cfname_fmt%5$cdb %2$cSully_%%d.s%2$c, 0%1$c%5$ccmd_fmt%5$c%5$cdb %2$cnasm -f elf64 Sully_%%1$d.s -o Sully_%%1$d.o && cc -no-pie Sully_%%1$d.o -o Sully_%%1$d && rm Sully_%%1$d.o%2$c, 0%1$c%5$cexec_fmt%5$cdb %2$c./Sully_%%d%2$c, 0%1$c%5$cstring%5$c%5$cdb %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c%5$cglobal main%1$c%1$cmain:%1$c%5$csub%5$crsp, 200%1$c%5$cmov%5$cr14, %4$d%1$c%1$c%5$ctest%5$cr14, r14%1$c%5$cjs%5$c.end%1$c%1$c%5$clea%5$crdi, [rsp + 8]%1$c%5$clea%5$crsi, [rel fname_fmt]%1$c%5$cmov%5$crdx, r14%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csprintf wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 8]%1$c%5$clea%5$crsi, [rel mode]%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$cfopen wrt ..plt%1$c%5$cmov%5$cr13, rax%1$c%5$ctest%5$cr13, r13%1$c%5$cjz%5$c.end%1$c%1$c%5$cmov%5$crdi, r13%1$c%5$clea%5$crsi, [rel string]%1$c%5$cmov%5$crdx, NL%1$c%5$cmov%5$crcx, QUOTE%1$c%5$clea%5$cr8, [rel string]%1$c%5$cmov%5$cr9, r14%1$c%5$cmov%5$cqword [rsp], TAB%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$cfprintf wrt ..plt%1$c%1$c%5$cmov%5$crdi, r13%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$cfclose wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 40]%1$c%5$clea%5$crsi, [rel cmd_fmt]%1$c%5$cmov%5$crdx, r14%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csprintf wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 40]%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csystem wrt ..plt%1$c%1$c%5$ctest%5$cr14, r14%1$c%5$cjz%5$c.end%1$c%1$c%5$cmov%5$cr15, r14%1$c%5$cdec%5$cr15%1$c%1$c%5$clea%5$crdi, [rsp + 8]%1$c%5$clea%5$crsi, [rel fname_fmt]%1$c%5$cmov%5$crdx, r15%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csprintf wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 8]%1$c%5$clea%5$crsi, [rel mode]%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$cfopen wrt ..plt%1$c%5$cmov%5$cr13, rax%1$c%5$ctest%5$cr13, r13%1$c%5$cjz%5$c.end%1$c%1$c%5$cmov%5$crdi, r13%1$c%5$clea%5$crsi, [rel string]%1$c%5$cmov%5$crdx, NL%1$c%5$cmov%5$crcx, QUOTE%1$c%5$clea%5$cr8, [rel string]%1$c%5$cmov%5$cr9, r15%1$c%5$cmov%5$cqword [rsp], TAB%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$cfprintf wrt ..plt%1$c%1$c%5$cmov%5$crdi, r13%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$cfclose wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 40]%1$c%5$clea%5$crsi, [rel cmd_fmt]%1$c%5$cmov%5$crdx, r15%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csprintf wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 40]%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csystem wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 168]%1$c%5$clea%5$crsi, [rel exec_fmt]%1$c%5$cmov%5$crdx, r15%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csprintf wrt ..plt%1$c%1$c%5$clea%5$crdi, [rsp + 168]%1$c%5$cxor%5$crax, rax%1$c%5$ccall%5$csystem wrt ..plt%1$c%1$c.end:%1$c%5$cadd%5$crsp, 200%1$c%5$cxor%5$crdi, rdi%1$c%5$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c", 0
+    s: db "; i = %2$d%1$cextern fopen, fprintf, fclose, sprintf, access, system%1$c%1$csection .data%1$c    s: db %3$c%4$s%3$c, 0%1$c    file_name: db %3$cSully_%%d.s%3$c, 0%1$c    mode: db %3$cw%3$c, 0%1$c    cmd: db %3$cnasm -f elf64 Sully_%%1$d.s -o Sully_%%1$d.o && gcc Sully_%%1$d.o -o Sully_%%1$d && rm Sully_%%1$d.o && ./Sully_%%1$d%3$c, 0%1$c%1$csection .bss%1$c    buf: resb 200%1$c%1$csection .text%1$c    global main%1$c%1$cmain:%1$c    push r12%1$c    push r13%1$c    mov r12d, %2$d%1$c    lea rdi, [rel buf]%1$c    lea rsi, [rel file_name]%1$c    mov edx, r12d%1$c    xor eax, eax%1$c    call sprintf wrt ..plt%1$c    lea rdi, [rel buf]%1$c    xor esi, esi%1$c    call access wrt ..plt%1$c    test eax, eax%1$c    jnz .next%1$c    dec r12d%1$c%1$c.next:%1$c    cmp r12d, 0%1$c    jl .done%1$c    lea rdi, [rel buf]%1$c    lea rsi, [rel file_name]%1$c    mov edx, r12d%1$c    xor eax, eax%1$c    call sprintf wrt ..plt%1$c    lea rdi, [rel buf]%1$c    lea rsi, [rel mode]%1$c    call fopen wrt ..plt%1$c    mov r13, rax%1$c    mov rdi, r13%1$c    lea rsi, [rel s]%1$c    mov edx, 10%1$c    mov ecx, r12d%1$c    mov r8d, 34%1$c    lea r9, [rel s]%1$c    xor eax, eax%1$c    call fprintf wrt ..plt%1$c    mov rdi, r13%1$c    call fclose wrt ..plt%1$c    lea rdi, [rel buf]%1$c    lea rsi, [rel cmd]%1$c    mov edx, r12d%1$c    xor eax, eax%1$c    call sprintf wrt ..plt%1$c    lea rdi, [rel buf]%1$c    call system wrt ..plt%1$c%1$c.done:%1$c    pop r13%1$c    pop r12%1$c    xor eax, eax%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c", 0
+    file_name: db "Sully_%d.s", 0
+    mode: db "w", 0
+    cmd: db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o && gcc Sully_%1$d.o -o Sully_%1$d && rm Sully_%1$d.o && ./Sully_%1$d", 0
+
+section .bss
+    buf: resb 200
 
 section .text
-	global main
+    global main
 
 main:
-	sub	rsp, 200
-	mov	r14, 5
+    push r12
+    push r13
+    mov r12d, 5
+    lea rdi, [rel buf]
+    lea rsi, [rel file_name]
+    mov edx, r12d
+    xor eax, eax
+    call sprintf wrt ..plt
+    lea rdi, [rel buf]
+    xor esi, esi
+    call access wrt ..plt
+    test eax, eax
+    jnz .next
+    dec r12d
 
-	test	r14, r14
-	js	.end
+.next:
+    cmp r12d, 0
+    jl .done
+    lea rdi, [rel buf]
+    lea rsi, [rel file_name]
+    mov edx, r12d
+    xor eax, eax
+    call sprintf wrt ..plt
+    lea rdi, [rel buf]
+    lea rsi, [rel mode]
+    call fopen wrt ..plt
+    mov r13, rax
+    mov rdi, r13
+    lea rsi, [rel s]
+    mov edx, 10
+    mov ecx, r12d
+    mov r8d, 34
+    lea r9, [rel s]
+    xor eax, eax
+    call fprintf wrt ..plt
+    mov rdi, r13
+    call fclose wrt ..plt
+    lea rdi, [rel buf]
+    lea rsi, [rel cmd]
+    mov edx, r12d
+    xor eax, eax
+    call sprintf wrt ..plt
+    lea rdi, [rel buf]
+    call system wrt ..plt
 
-	lea	rdi, [rsp + 8]
-	lea	rsi, [rel fname_fmt]
-	mov	rdx, r14
-	xor	rax, rax
-	call	sprintf wrt ..plt
-
-	lea	rdi, [rsp + 8]
-	lea	rsi, [rel mode]
-	xor	rax, rax
-	call	fopen wrt ..plt
-	mov	r13, rax
-	test	r13, r13
-	jz	.end
-
-	mov	rdi, r13
-	lea	rsi, [rel string]
-	mov	rdx, NL
-	mov	rcx, QUOTE
-	lea	r8, [rel string]
-	mov	r9, r14
-	mov	qword [rsp], TAB
-	xor	rax, rax
-	call	fprintf wrt ..plt
-
-	mov	rdi, r13
-	xor	rax, rax
-	call	fclose wrt ..plt
-
-	lea	rdi, [rsp + 40]
-	lea	rsi, [rel cmd_fmt]
-	mov	rdx, r14
-	xor	rax, rax
-	call	sprintf wrt ..plt
-
-	lea	rdi, [rsp + 40]
-	xor	rax, rax
-	call	system wrt ..plt
-
-	test	r14, r14
-	jz	.end
-
-	mov	r15, r14
-	dec	r15
-
-	lea	rdi, [rsp + 8]
-	lea	rsi, [rel fname_fmt]
-	mov	rdx, r15
-	xor	rax, rax
-	call	sprintf wrt ..plt
-
-	lea	rdi, [rsp + 8]
-	lea	rsi, [rel mode]
-	xor	rax, rax
-	call	fopen wrt ..plt
-	mov	r13, rax
-	test	r13, r13
-	jz	.end
-
-	mov	rdi, r13
-	lea	rsi, [rel string]
-	mov	rdx, NL
-	mov	rcx, QUOTE
-	lea	r8, [rel string]
-	mov	r9, r15
-	mov	qword [rsp], TAB
-	xor	rax, rax
-	call	fprintf wrt ..plt
-
-	mov	rdi, r13
-	xor	rax, rax
-	call	fclose wrt ..plt
-
-	lea	rdi, [rsp + 40]
-	lea	rsi, [rel cmd_fmt]
-	mov	rdx, r15
-	xor	rax, rax
-	call	sprintf wrt ..plt
-
-	lea	rdi, [rsp + 40]
-	xor	rax, rax
-	call	system wrt ..plt
-
-	lea	rdi, [rsp + 168]
-	lea	rsi, [rel exec_fmt]
-	mov	rdx, r15
-	xor	rax, rax
-	call	sprintf wrt ..plt
-
-	lea	rdi, [rsp + 168]
-	xor	rax, rax
-	call	system wrt ..plt
-
-.end:
-	add	rsp, 200
-	xor	rdi, rdi
-	ret
+.done:
+    pop r13
+    pop r12
+    xor eax, eax
+    ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
--- C/Sully.c
+++ C/Sully.c
@@ -1,40 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
 	int nb = 5;
-	char filename[20];
-	char cmd[50];
-	char exec[20];
-	char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main()%1$c{%1$c%2$cint nb = %3$d;%1$c%2$cchar filename[20];%1$c%2$cchar cmd[50];%1$c%2$cchar exec[20];%1$c%2$cchar *s = %4$c%5$s%4$c;%1$c%2$csprintf(filename, %4$cSully_%%d.c%4$c, nb);%1$c%2$cFILE *test = fopen(filename, %4$cr%4$c);%1$c%2$cif (test == NULL)%1$c%2$c{%1$c%2$c%2$cFILE *f = fopen(filename, %4$cw%4$c);%1$c%2$c%2$cfprintf(f, s, 10, 9, nb, 34, s);%1$c%2$c%2$cfclose(f);%1$c%2$c%2$csprintf(cmd, %4$cgcc -o Sully_%%d Sully_%%d.c%4$c, nb, nb);%1$c%2$c%2$csystem(cmd);%1$c%2$c%2$csprintf(exec, %4$c./Sully_%%d%4$c, nb);%1$c%2$c%2$csystem(exec);%1$c%2$c}%1$c%2$celse%1$c%2$c{%1$c%2$c%2$cfclose(test);%1$c%2$c%2$cif (nb - 1 >= 0)%1$c%2$c%2$c{%1$c%2$c%2$c%2$csprintf(filename, %4$cSully_%%d.c%4$c, nb - 1);%1$c%2$c%2$c%2$cFILE *f = fopen(filename, %4$cw%4$c);%1$c%2$c%2$c%2$cfprintf(f, s, 10, 9, nb - 1, 34, s);%1$c%2$c%2$c%2$cfclose(f);%1$c%2$c%2$c%2$csprintf(cmd, %4$cgcc -o Sully_%%d Sully_%%d.c%4$c, nb - 1, nb - 1);%1$c%2$c%2$c%2$csystem(cmd);%1$c%2$c%2$c%2$csprintf(exec, %4$c./Sully_%%d%4$c, nb - 1);%1$c%2$c%2$c%2$csystem(exec);%1$c%2$c%2$c}%1$c%2$c}%1$c%2$creturn (0);%1$c}";
-
-	sprintf(filename, "Sully_%d.c", nb);
-	FILE *test = fopen(filename, "r");
-	if (test == NULL)
-	{
-		FILE *f = fopen(filename, "w");
-		fprintf(f, s, 10, 9, nb, 34, s);
-		fclose(f);
-		sprintf(cmd, "gcc -o Sully_%d Sully_%d.c", nb, nb);
-		system(cmd);
-		sprintf(exec, "./Sully_%d", nb);
-		system(exec);
-	}
-	else
-	{
-		fclose(test);
-		if (nb - 1 >= 0)
-		{
-			sprintf(filename, "Sully_%d.c", nb - 1);
-			FILE *f = fopen(filename, "w");
-			fprintf(f, s, 10, 9, nb - 1, 34, s);
-			fclose(f);
-			sprintf(cmd, "gcc -o Sully_%d Sully_%d.c", nb - 1, nb - 1);
-			system(cmd);
-			sprintf(exec, "./Sully_%d", nb - 1);
-			system(exec);
-		}
-	}
+	char buf[50];
+	char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c%1$cint main()%1$c{%1$c%2$cint nb = %3$d;%1$c%2$cchar buf[300];%1$c%2$cchar *s = %4$c%5$s%4$c;%1$c%2$csprintf(buf, %4$cSully_%%d.c%4$c, nb);%1$c%2$cif (fopen(buf, %4$cr%4$c))%1$c%2$c%2$cnb--;%1$c%2$cif (nb < 0)%1$c%2$c%2$creturn (0);%1$c%2$csprintf(buf, %4$cSully_%%d.c%4$c, nb);%1$c%2$cFILE *f = fopen(buf, %4$cw%4$c);%1$c%2$cfprintf(f, s, 10, 9, nb, 34, s);%1$c%2$cfclose(f);%1$c%2$csprintf(buf, %4$cgcc -o Sully_%%1$d Sully_%%1$d.c && ./Sully_%%1$d%4$c, nb);%1$c%2$csystem(buf);%1$c%2$creturn (0);%1$c}";
+	sprintf(buf, "Sully_%d.c", nb);
+	if (access(buf, F_OK) == 0)
+		nb--;
+	if (nb < 0)
+		return (0);
+	sprintf(buf, "Sully_%d.c", nb);
+	FILE *f = fopen(buf, "w");
+	fprintf(f, s, 10, 9, nb, 34, s);
+	fclose(f);
+	sprintf(buf, "gcc -o Sully_%1$d Sully_%1$d.c && ./Sully_%1$d", nb);
+	system(buf);
 	return (0);
 }
